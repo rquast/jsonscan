4af6df7f8b339c1aa2c6c31908da9d3f2456a9f5
Roland Quast <rquast@rolands-macbook.local>
2016-04-14 07:09:43 +1000

    Adding help information.

--- jsonscan/main.m
+++ jsonscan/main.m
@@ -2,38 +2,53 @@
 #import "AppController.h"
 #import "JsonConfiguration.h"
 
+#define NSLog(FORMAT, ...) printf("%s\n", [[NSString stringWithFormat:FORMAT, ##__VA_ARGS__] UTF8String]);
+
 int main (int argc, const char * argv[])
 {
     
     @autoreleasepool {
         
         AppController* appController = [[AppController alloc] init];
-        
-        if (argc < 2) {
+
             
-            // TODO: display basic usage
+        NSString *inputString;
+        
+        if (argc == 2 && [[NSString stringWithCString:argv[1] encoding:NSUTF8StringEncoding] isEqualToString:@"-h"]) {
+            
+            NSDictionary * options = [JsonConfiguration getOptions];
+            
+            // TODO: display options usage.
+            
+        } else if (argc == 2 && [[NSString stringWithCString:argv[1] encoding:NSUTF8StringEncoding] isEqualToString:@"-l"]) {
+            
+            inputString = @"{\"action\": \"list\"}";
+            [appController exec:inputString];
+            
+        } else if (argc == 2 && [[NSString stringWithCString:argv[1] encoding:NSUTF8StringEncoding] isEqualToString:@"-c"]) {
+            
+            NSFileHandle *input = [NSFileHandle fileHandleWithStandardInput];
+            NSData *inputData = [NSData dataWithData:[input readDataToEndOfFile]];
+            inputString = [[NSString alloc] initWithData:inputData encoding:NSUTF8StringEncoding];
+            [appController exec:inputString];
+            
+        } else if (argc == 2 && [[NSString stringWithCString:argv[1] encoding:NSUTF8StringEncoding] isEqualToString:@"-s"]) {
+            
+            inputString = @"{\"action\": \"scan\"}";
+            [appController exec:inputString];
             
         } else {
             
-            NSString *inputString;
-            
-            if (argc == 2 && [[NSString stringWithCString:argv[1] encoding:NSUTF8StringEncoding] isEqualToString:@"-h"]) {
-
-                NSDictionary * options = [JsonConfiguration getOptions];
-                
-                // TODO: display options usage.
-                
-            } else if (argc == 2 && [[NSString stringWithCString:argv[1] encoding:NSUTF8StringEncoding] isEqualToString:@"-l"]) {
-                inputString = @"{\"action\": \"list\"}";
-            } else if (argc == 2 && [[NSString stringWithCString:argv[1] encoding:NSUTF8StringEncoding] isEqualToString:@"-c"]) {
-                NSFileHandle *input = [NSFileHandle fileHandleWithStandardInput];
-                NSData *inputData = [NSData dataWithData:[input readDataToEndOfFile]];
-                inputString = [[NSString alloc] initWithData:inputData encoding:NSUTF8StringEncoding];
-            } else if (argc == 2 && [[NSString stringWithCString:argv[1] encoding:NSUTF8StringEncoding] isEqualToString:@"-s"]) {
-                inputString = @"{\"action\": \"scan\"}";
-            }
-            
-            [appController exec:inputString];
+            NSLog(@"Usage: jsonscan [-option]");
+            NSLog(@"");
+            NSLog(@"-l : List all scanners and settings.");
+            NSLog(@"");
+            NSLog(@"-s : Perform a scan with the first available scanner.");
+            NSLog(@"");
+            NSLog(@"-c : Read JSON action from stdin. Example: jsonscan -c < action.json");
+            NSLog(@"");
+            NSLog(@"-h : List all setting keys and descriptions.");
+            NSLog(@"");
             
         }
         
